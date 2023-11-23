061f574854333620cddad7b401e57bf2c8bcdd8b
Mike Wyatt <wyatt.mike@gmail.com>
2023-11-23 17:14:02 -0400

    actually test export maps

---
+++ downstream-package/cjs-test.ts
@@ -0,0 +1,5 @@
+const { generateConfig } = require('esm-cjs-playground')
+const { add } = require('esm-cjs-playground/math')
+
+console.log(generateConfig())
+console.log(add(1, 2))
---
+++ downstream-package/esm-test.mts
@@ -0,0 +1,5 @@
+import { generateConfig } from 'esm-cjs-playground'
+import { add } from 'esm-cjs-playground/math'
+
+console.log(generateConfig())
+console.log(add(1, 2))
