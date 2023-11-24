cb877ddf54b039621251f90e5f68c487cb936432
Mike Wyatt <wyatt.mike@gmail.com>
2023-11-24 11:50:59 -0400

    but it can't handle directory imports!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import fromDirectory from './directory'
 import { add, multiply } from './math'
 
 export default function generateConfig() {
   return {
     a: add(12, 5),
     b: multiply(12, 5),
+    foo: fromDirectory(),
   }
 }
