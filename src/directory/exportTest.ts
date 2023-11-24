f3721be334746387cbb90f390bfa8305fe6c419a
Mike Wyatt <wyatt.mike@gmail.com>
2023-11-24 12:14:45 -0400

    and a simple export from shows a bug in attw https://github.com/arethetypeswrong/arethetypeswrong.github.io/issues/117

---
+++ src/directory/exportTest.ts
@@ -0,0 +1,3 @@
+export function exportTest() {
+  return 'exportTest'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import chalk from 'chalk'
+
 import fromDirectory from './directory'
 import { add, multiply } from './math'
 
+export { exportTest } from './directory/exportTest'
+
 export default function generateConfig() {
+  console.log(chalk.bold('hello world'))
+
   return {
     a: add(12, 5),
     b: multiply(12, 5),
