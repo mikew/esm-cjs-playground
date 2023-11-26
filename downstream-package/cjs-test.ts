3c380e37248a84d1790a757e769fcb0221bc39e8
Mike Wyatt <wyatt.mike@gmail.com>
2023-11-26 16:06:23 -0400

    attw still fails with a named export

--- downstream-package/cjs-test.ts
+++ downstream-package/cjs-test.ts
@@ -1,5 +1,6 @@
-import generateConfig from 'esm-cjs-playground'
+import generateConfig, { exportTest } from 'esm-cjs-playground'
 import { add } from 'esm-cjs-playground/math'
 
+console.log(exportTest())
 console.log(generateConfig())
 console.log(add(1, 2))
